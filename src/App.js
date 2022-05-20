412c59c97799c9cc2ef154ed2d32c1b588f98ca1
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-05-20 19:00:50 +0430

    set usestates to input and keep items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (event) => {
+    //we want to keep what we have, append new tems to the array
+    setTodos([...todos, input]);
+  };
   return (
-    <div className="todo-app">
+    <div className="App">
       <h1>ToDo App</h1>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={addTodo}>Add ToDo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
