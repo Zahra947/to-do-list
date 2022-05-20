13f71c4f6d5de09c5660fc47e6fc20bb167a28ab
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-05-20 19:39:14 +0430

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeAPP({
+  apiKey: "AIzaSyDEMn9-CmPMuWxQ1WPOfezyDruGoOVd6Xg",
+  authDomain: "to-do-app-4bc3f.firebaseapp.com",
+  projectId: "to-do-app-4bc3f",
+  storageBucket: "to-do-app-4bc3f.appspot.com",
+  messagingSenderId: "1092825749742",
+  appId: "1:1092825749742:web:3b198c3417ce62482dbbff",
+  measurementId: "G-CKQ1EN5YK7"
+});
