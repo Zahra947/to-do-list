6b3a88ba2674219fb988537aaab44f470e2af04b
Zahra Rabiei <zhr.rabiei@gmail.com>
2022-06-18 13:36:24 +0430

    added comments to make the code readible

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,6 +9,12 @@ export default function TodoList() {
   const addTodo = (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
+      {/* explaining of the second expression: ^: the beginning of the line of data, 
+        $ :followed by the end of the line of data
+        \s: stands for whitespace character
+        * : 0 or more of the charachter before *
+        /EXPRESSION /: // means does the expression mathches with
+        /^\s*$/.test(val): uses RegExp test method to test whether a string val is empty or only contains spaces*/}
     }
     const newTodos = [todo, ...todos];
     setTodos(newTodos);
@@ -40,7 +46,7 @@ export default function TodoList() {
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
-      <TodoForm onSubmit={addTodo} />
+      <TodoForm onSubmit={addTodo} />{/*the function is defined in parent component(here) and like a prop will be used in child component */}
       <Todo
         todos={todos}
         completeTodo={completeTodo}
